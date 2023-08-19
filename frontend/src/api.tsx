e1f8d05b11b021989b8b557e70ccf346d223696c
teddysmithdev <teddysmithdev@gmail.com>
2023-08-19 10:37:06 -0500

    Axios

---
+++ frontend/src/api.tsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { CompanySearch } from "./company";
+
+interface SearchResponse {
+  data: CompanySearch[];
+}
+
+export const searchCompanies = async (query: string) => {
+  try {
+    const data = await axios.get<SearchResponse>(
+      `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("error message: ", error.message);
+      return error.message;
+    } else {
+      console.log("unexpected error: ", error);
+      return "An expected error has occured.";
+    }
+  }
+};
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { searchCompanies } from "./api";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+//console.log(searchCompanies("tsla"));
 root.render(
   <React.StrictMode>
     <App />
