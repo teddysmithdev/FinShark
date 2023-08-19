d35156e60f76472c67fc6bdeace86942bd43146f
teddysmithdev <teddysmithdev@gmail.com>
2023-08-19 07:55:26 -0500

    Events

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,17 +1,22 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useState, SyntheticEvent } from "react";
 
 type Props = {};
 
 const Search: React.FC<Props> = (props: Props): JSX.Element => {
   const [search, setSearch] = useState<string>("");
 
-  const onClick = (e: any) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
+    console.log(e);
+  };
+
+  const onClick = (e: SyntheticEvent) => {
     console.log(e);
   };
   return (
     <div>
-      <input value={search} onChange={(e) => onClick(e)}></input>
+      <input value={search} onChange={(e) => handleChange(e)}></input>
+      <button onClick={(e) => onClick(e)} />
     </div>
   );
 };
