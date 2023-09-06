c250fd51dfcc6b86cea5c5d612546588a6efd8d2
teddysmithdev <teddysmithdev@gmail.com>
2023-09-06 08:34:47 -0500

    useEffect

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { CompanyProfile } from "../../company";
+import { useParams } from "react-router-dom";
+import { getCompanyProfile } from "../../api";
 
 interface Props {}
 
 const CompanyPage = (props: Props) => {
-  return <div>CompanyPage</div>;
+  let { ticker } = useParams();
+  const [company, setCompany] = useState<CompanyProfile>();
+
+  useEffect(() => {
+    const getProfileInit = async () => {
+      const result = await getCompanyProfile(ticker!);
+      setCompany(result?.data[0]);
+    };
+    getProfileInit();
+  }, []);
+
+  return (
+    <>
+      {company ? (
+        <div className="company-profile-container">{company.companyName}</div>
+      ) : (
+        <div>Company Not Found!</div>
+      )}
+    </>
+  );
 };
 
 export default CompanyPage;
--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CompanySearch } from "./company";
+import { CompanyProfile, CompanySearch } from "./company";
 
 export interface SearchResponse {
   data: CompanySearch[];
@@ -19,5 +19,16 @@ export const searchCompanies = async (query: string) => {
       console.log("unexpected error: ", error);
       return "An expected error has occured.";
     }
+  }
+};
+
+export const getCompanyProfile = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyProfile[]>(
+      `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message: ", error.message);
   }
 };
