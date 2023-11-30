8f3404331bd941b124e6633850700c8821f5a479
teddysmithdev <teddysmithdev@gmail.com>
2023-11-30 11:56:16 -0500

    Models

---
+++ api/Models/Comment.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int? StockId { get; set; }
+        public Stock? Stock { get; set; }
+    }
+}
---
+++ api/Models/Stock.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Purchase { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal LastDiv { get; set; }
+        public string Industry { get; set; } = string.Empty;
+        public long MarketCap { get; set; }
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+    }
+}
