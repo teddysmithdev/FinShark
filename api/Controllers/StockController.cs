d28c8d1e91205f296c223b838da8987b03a44bed
teddysmithdev <teddysmithdev@gmail.com>
2023-12-05 08:38:51 -0500

    Delete

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -73,5 +73,23 @@ namespace api.Controllers
             return Ok(stockModel.ToStockDto());
         }
 
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stocks.Remove(stockModel);
+
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
     }
 }
