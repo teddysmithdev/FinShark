f599cebcf0c2419e47c45053788032fe1363bde5
teddysmithdev <teddysmithdev@gmail.com>
2023-12-27 10:50:32 -0500

    Pagination

--- api/Helpers/QueryObject.cs
+++ api/Helpers/QueryObject.cs
@@ -11,5 +11,7 @@ namespace api.Helpers
         public string? CompanyName { get; set; } = null;
         public string? SortBy { get; set; } = null;
         public bool IsDecsending { get; set; } = false;
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
     }
 }
--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -62,7 +62,10 @@ namespace api.Repository
                 }
             }
 
-            return await stocks.ToListAsync();
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
         public async Task<Stock?> GetByIdAsync(int id)
