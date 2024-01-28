feceb2e3ce5a4f187122ddeb8615792641a67b0d
teddysmithdev <teddysmithdev@gmail.com>
2024-01-28 12:12:49 -0500

    CORS

--- api/Program.cs
+++ api/Program.cs
@@ -108,6 +108,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials()
+    //.WithOrigins("https://localhost:44351)
+    .SetIsOriginAllowed(origin => true));
+
 app.UseAuthentication();
 app.UseAuthorization();
 
